efe2cddff18c194e3a6f675962cf71cc5da9ade3
listen <listen2code@gmail.com>
2021-02-15 15:17:27 +0800

    test: plugin的Activity 替换成 AppCompatActivity

--- projects/sample/maven/plugin-project/plugin-main-lib2/src/main/java/com/tencent/shadow/sample/plugin2/MainActivity.java
+++ projects/sample/maven/plugin-project/plugin-main-lib2/src/main/java/com/tencent/shadow/sample/plugin2/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tencent.shadow.sample.plugin2;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
